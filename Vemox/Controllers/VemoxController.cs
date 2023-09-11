3ca1c10453f7007856bf5e0fde6002b32f1c008e
USer <USer@DESKTOP-3ACNRK3>
2023-09-12 05:06:27 +0700

    Add project files.

---
+++ Vemox/Controllers/VemoxController.cs
@@ -0,0 +1,196 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Vemox.Controllers
+{
+    public class VemoxController : Controller
+    {
+       
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SubmitForm(string inputValue, string language)
+        {
+            if (language == "SQL")
+            {
+                // ส่ง inputValue ไปยังแอคชัน "SQLresult"
+                return RedirectToAction("SQLresult", new { inputValue = inputValue });
+            }
+            else if (language == "MQL")
+            {
+                // ส่ง inputValue ไปยังแอคชัน "MQLresult"
+                return RedirectToAction("MQLresult", new { inputValue = inputValue });
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+
+
+
+
+        public IActionResult SQLresult(string inputValue)
+        {
+            string sqlinput;
+            int num1;
+            int num2 = 0;
+            Stopwatch stopwatch = new Stopwatch();
+
+
+            if (inputValue.EndsWith(";"))
+            {
+                sqlinput = inputValue.Replace(";", "");
+                string[] words = sqlinput.Split(' ');
+
+                if (words[0].Equals("SELECT"))
+                {
+                    if (int.TryParse(words[1], out num1))
+                    {
+                        if (words.Length >= 3 && (words[2].Equals("+"))) 
+                        {
+                            if (int.TryParse(words[3], out num2)) 
+                            {
+                                stopwatch.Start();
+                                ViewBag.outputQuery = inputValue;
+                                ViewBag.outputResult = num1+num2;
+                                ViewBag.status = "true";
+                                stopwatch.Stop();
+                                TimeSpan elapsedTime = stopwatch.Elapsed;
+                                long milliseconds = (long)elapsedTime.TotalMilliseconds;
+                                ViewBag.time = milliseconds;
+
+
+                                return View();
+                            }
+                            else 
+                            {
+                                return RedirectToAction("resultwong");
+                            }
+                        }
+                        else
+                        {
+                            stopwatch.Start();
+                            ViewBag.outputQuery = inputValue;
+                            ViewBag.outputResult = num1;
+                            ViewBag.status = "true";
+                            stopwatch.Stop();
+                            TimeSpan elapsedTime = stopwatch.Elapsed;
+                            long milliseconds = (long)elapsedTime.TotalMilliseconds;
+                            ViewBag.time = milliseconds;
+
+                            return View();
+                        }
+
+                            
+                        
+                    }
+                    else
+                    {
+                        return RedirectToAction("resultwong");
+                    }
+                }
+                else
+                {
+                    return RedirectToAction("resultwong");
+                }
+            }
+            else
+            {
+                return RedirectToAction("resultwong");
+            }
+          
+                
+        }
+        public IActionResult resultwong()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult MQLresult(string inputValue)
+        {
+            string mqlinput;
+            int num1;
+            int num2 = 0;
+            Stopwatch stopwatch = new Stopwatch();
+
+
+            if (inputValue.StartsWith("print(") && inputValue.EndsWith(");"))
+            {
+                mqlinput = inputValue.Replace(";", " ");
+                mqlinput = mqlinput.Replace("(", " ");
+                mqlinput = mqlinput.Replace(")", " ");
+                string[] words = mqlinput.Split(' ');
+
+                if (words[0].Equals("print"))
+                {
+                    if (int.TryParse(words[1], out num1))
+                    {
+                        if (words.Length >= 3 && (words[2].Equals("+")))
+                        {
+                            if (int.TryParse(words[3], out num2))
+                            {
+                                stopwatch.Start();
+                                ViewBag.outputQuery = inputValue;
+                                ViewBag.outputResult = num1 + num2;
+                                ViewBag.status = "true";
+                                stopwatch.Stop();
+                                TimeSpan elapsedTime = stopwatch.Elapsed;
+                                long milliseconds = (long)elapsedTime.TotalMilliseconds;
+                                ViewBag.time = milliseconds;
+
+
+                                return View();
+                            }
+                            else
+                            {
+                                return RedirectToAction("resultwong");
+
+                            }
+                        }
+                        else
+                        {
+                            stopwatch.Start();
+                            ViewBag.outputQuery = inputValue;
+                            ViewBag.outputResult = num1;
+                            ViewBag.status = "true";
+                            stopwatch.Stop();
+                            TimeSpan elapsedTime = stopwatch.Elapsed;
+                            long milliseconds = (long)elapsedTime.TotalMilliseconds;
+                            ViewBag.time = milliseconds;
+
+                            return View();
+                        }
+
+
+
+                    }
+                    else
+                    {
+                        return RedirectToAction("resultwong");
+
+                    }
+                }
+                else
+                {
+                    ViewBag.outputQuery = words[0];
+                                return View(); ;
+                }
+            }
+            else
+            {
+                return RedirectToAction("resultwong");
+
+            }
+
+
+        }
+        
+    }
+}
---
+++ Vemox/Models/Vemoxs.cs
@@ -0,0 +1,13 @@
+﻿namespace Vemox.Models
+{
+    public class Vemoxs
+    {
+
+        public string inputValue { get; set; }
+        public string outputQuery { get; set; }
+        public int outputResult { get; set; }
+        public long time { get; set; }
+
+
+    }
+}
